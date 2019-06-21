1c09d7edf29bf566f481a6fcd5571442a28c59ca
marcinmatuk <mmatuk@hawk.iit.edu>
2019-06-20 21:05:56 -0500

    Added login and log out functionality.

--- DataCommunicationLayer.py
+++ DataCommunicationLayer.py
@@ -1,6 +1,6 @@
 import psycopg2 as pg
-
-
+from werkzeug.security import generate_password_hash
+from User import User, salt
 
 db_host = "localhost"
 database = "test"
@@ -21,12 +21,15 @@ class DataCommunicationLayer:
             self._logger.info(e)
             Exception('Connecting to the Database Failed!')
 
-    def register_user(self,email,name,home_airport = None):
+    def register_user(self,email,password,name,home_airport = None):
         cursor = self._db_conn.cursor()
         success = True
+        saltedhash = generate_password_hash(salt + password)
         try:
             values = cursor.mogrify("(%s,%s,%s)", (email,name,home_airport)).decode('utf-8')
             cursor.execute('insert into customer values ' + values)
+            values = cursor.mogrify("(%s,%s)", (email, saltedhash)).decode('utf-8')
+            cursor.execute('insert into registered_user values ' + values)
             self._db_conn.commit()
             self._logger.info('Successfully inserted %s into the Database.' % email)
         except Exception as e:
@@ -35,3 +38,13 @@ class DataCommunicationLayer:
         finally:
             cursor.close()
             return success
+
+    def get_user_from_db(self,id):
+        cursor = self._db_conn.cursor()
+        sql = 'select * from registered_user where email = %(user_id)s'
+        cursor.execute(sql,{'user_id':id})
+        rows = cursor.fetchall()
+        if len(rows) == 0:
+            return None
+        else:
+            return User(id,rows[0][1])
--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from flask import session
+from flask_login import current_user, login_user, LoginManager, logout_user
+from User import User
 from flask import render_template
 from flask import request
+import wtforms
+from flask import redirect, url_for
 from flask.json import dumps
 from log import setup_logging
 from DataCommunicationLayer import DataCommunicationLayer
@@ -11,6 +15,9 @@ app = Flask(__name__)
 
 async_mode = None
 app.secret_key = "#$ALKJ34"
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 @app.route("/")
 def landing_page():
@@ -24,12 +31,14 @@ def landing_page():
 def registration_page():
     return render_template("UserRegistration.html")
 
+
 @app.route("/register_user",methods = ["POST"])
 def register_user():
 
     data = json.loads(request.data.decode('utf-8').replace("'", '"'))
 
     email = data['email']
+    password = data['password']
     name = data['name']
     home_airport = data['home_airport']
 
@@ -38,12 +47,44 @@ def register_user():
     if name == '':
         return 'Name cannot be blank!'
     if home_airport == '':
-        success = comm_layer.register_user(email,name)
+        success = comm_layer.register_user(email,password,name)
     else:
-        success = comm_layer.register_user(email, name, home_airport)
+        success = comm_layer.register_user(email,password, name, home_airport)
     return 'Successfully Added new User' if success else 'Failed to Add new User!'
 
+@login_manager.user_loader
+def load_user(user_id):
+    return comm_layer.get_user_from_db(user_id)
 
+
+@app.route('/login_page', methods=['POST'])
+def login_page():
+    if current_user.is_authenticated:
+        return 'User id already logged in!'
+    return render_template("login.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    data = json.loads(request.data.decode('utf-8').replace("'", '"'))
+    email = data['email']
+    pwd = data['password']
+
+    user = comm_layer.get_user_from_db(email)
+    if user is None or not user.check_password(pwd):
+        return 'Invalid username or password'
+        #return redirect(url_for('login'))
+    login_user(user)
+    return 'Successfully Logged In!'
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    logout_user()
+    return 'Successfully Logged Out!'
 
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
+
+
